eb6e5b1afd3d339a0f362cd2d2cad11ef16cc8a9
prathyushab14 <prathyushabathula14@msitprogram.net>
2018-08-12 17:57:29 +0530

    " -> 0 of 4 passed. pylint: 6.96/10 "

--- p1/create_social_network.py
+++ p1/create_social_network.py
@@ -1,19 +1,21 @@
 """sco"""
 def create_social_network(data):
     """net"""
-    data = data.split(" follows ")
     dict1 = {}
-    def sns(data)
-    data = data.split(" follows ")
-    if i not in dict1:
-        dict1[data[0]] = data[1].split(",")
-    else:
-        dict1[data[0]] += data[1].split(",")
-return dict1
-splitline = data.splitlines()
-for i in splitline:
-    sns(i)
-print(dict1)
+    def slomosplit(data):
+        data = data.split(" follows ")
+        if data[0] not in dict1:
+            dict1[data[0]] = data[1].split(',')
+        else:
+            #print(data)
+            dict1[data[0]] += data[1].split(',')
+        return dict1
+    splitline = data.splitlines()
+    for i in splitline:
+        slomosplit(i)
+    return dict1
+
+
 def main():
     """handling test cases"""
     string = ''
@@ -25,3 +27,4 @@ def main():
     print(create_social_network(string))
 if __name__ == "__main__":
     main()
+s
